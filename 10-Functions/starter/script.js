219456ac564b7a7f4a4d50c1a5be6060e9f69768
Simeon Raychev <simeon0022@gmail.com>
2022-07-01 12:10:56 +0300

    All solutions and tasks from my programming course are presented here

---
+++ 10-Functions/starter/script.js
@@ -0,0 +1,10 @@
+document.getElementById("option-1").addEventListener("click", () => {
+	alert('Option-1 was clicked');
+});
+document.getElementById("option-2").addEventListener("click", () => {
+	alert('Option-2 was clicked');
+});
+
+document.getElementById("option-3").addEventListener("click", () => {
+	alert('Option-3 was clicked');
+});
