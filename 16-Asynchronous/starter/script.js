06707c252fee97843657db1e1dea2672a34e1865
Simeon Raychev <simeon0022@gmail.com>
2022-07-03 13:31:53 +0300

    Promises Theory written

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -5,33 +5,33 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.eu/rest/v2/name/portugal');
-request.send();
+// const request = new XMLHttpRequest();
+// request.open('GET', 'https://restcountries.eu/rest/v2/name/portugal');
+// request.send();
 
-request.addEventListener('load', function(){
-    let [data] =  JSON.parse(this.responseText);
-    console.log(data);
+// request.addEventListener('load', function(){
+//     let [data] =  JSON.parse(this.responseText);
+//     console.log(data);
 
-  const html = `
-  <article class="country ${className}">
-    <img class="country__img" src="${data.capital}" />
-    <div class="country__data">
-      <h3 class="country__name">${data.name.common}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(
-        +data.population / 1000000
-      ).toFixed(1)} people</p>
-      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-    </div>
-  </article>
-  `;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
+//   const html = `
+//   <article class="country ${className}">
+//     <img class="country__img" src="${data.capital}" />
+//     <div class="country__data">
+//       <h3 class="country__name">${data.name.common}</h3>
+//       <h4 class="country__region">${data.region}</h4>
+//       <p class="country__row"><span>👫</span>${(
+//         +data.population / 1000000
+//       ).toFixed(1)} people</p>
+//       <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//       <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//     </div>
+//   </article>
+//   `;
+//   countriesContainer.insertAdjacentHTML('beforeend', html);
+//   countriesContainer.style.opacity = 1;
 
 
-});
+// });
 
 /* <article class="country">
             <img class="country__img" src="${data.flag}" />
@@ -44,3 +44,14 @@ request.addEventListener('load', function(){
             </div>
             </article>
             `; */
+
+
+const request = fetch('https://restcountries.eu/rest/v2/name/portugal');
+console.log(request);
+
+//What is a promise? - An object then is used as a placeholder for the future result of an async operation/ a containter for a async delivered value
+ // ++ we don't need to rely on events and callbacks 
+ // instead of nesting callbacks, we can chain promises 
+ // The promise LIFECYCLE ->> PENDING - before the value is available ; SETTLED - async task has finished (They are 2 types : FULFILLED - Success! , REJECTED - An error happened!); !!!WE ARE ABLE TO HANDLE THOSE STATES OF THE OPERATION IN OUR CODE
+
+ 
