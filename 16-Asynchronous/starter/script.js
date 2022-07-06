e23b738b01af66abc46a8bec320bbb7646ddaa91
Simeon Raychev <simeon0022@gmail.com>
2022-07-06 15:53:42 +0300

    GetCurrentPosition Promise

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -275,53 +275,64 @@ GOOD LUCK 😀
 
 
 // Building a Simple Promise
-const lotteryPromise = new Promise(function (resolve, reject) {
-  console.log('Lotter draw is happening 🔮');
-  setTimeout(function () {
-    if (Math.random() >= 0.5) {
-      resolve('You WIN 💰');
-    } else {
-      reject(new Error('You lost your money 💩'));
-    }
-  }, 2000);
-});
+// const lotteryPromise = new Promise(function (resolve, reject) {
+//   console.log('Lotter draw is happening 🔮');
+//   setTimeout(function () {
+//     if (Math.random() >= 0.5) {
+//       resolve('You WIN 💰');
+//     } else {
+//       reject(new Error('You lost your money 💩'));
+//     }
+//   }, 2000);
+// });
 
-lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+// lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
 
-// Promisifying setTimeout
-const wait = function (seconds) {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, seconds * 1000);
-  });
-};
+// // Promisifying setTimeout
+// const wait = function (seconds) {
+//   return new Promise(function (resolve) {
+//     setTimeout(resolve, seconds * 1000);
+//   });
+// };
 
-wait(1)
-  .then(() => {
-    console.log('1 second passed');
-    return wait(1);
-  })
-  .then(() => {
-    console.log('2 second passed');
-    return wait(1);
-  })
-  .then(() => {
-    console.log('3 second passed');
-    return wait(1);
-  })
-  .then(() => console.log('4 second passed'));
+// wait(1)
+//   .then(() => {
+//     console.log('1 second passed');
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log('2 second passed');
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log('3 second passed');
+//     return wait(1);
+//   })
+//   .then(() => console.log('4 second passed'));
 
-    setTimeout(() => {
-      console.log('1 second passed');
-      setTimeout(() => {
-        console.log('2 seconds passed');
-        setTimeout(() => {
-          console.log('3 second passed');
-          setTimeout(() => {
-            console.log('4 second passed');
-          }, 1000);
-        }, 1000);
-      }, 1000);
-    }, 1000);
+//     setTimeout(() => {
+//       console.log('1 second passed');
+//       setTimeout(() => {
+//         console.log('2 seconds passed');
+//         setTimeout(() => {
+//           console.log('3 second passed');
+//           setTimeout(() => {
+//             console.log('4 second passed');
+//           }, 1000);
+//         }, 1000);
+//       }, 1000);
+//     }, 1000);
 
-Promise.resolve('abc').then(x => console.log(x));
-Promise.reject(new Error('Problem!')).catch(x => console.error(x));
+// Promise.resolve('abc').then(x => console.log(x));
+// Promise.reject(new Error('Problem!')).catch(x => console.error(x));
+
+  const getPosition = function(){
+    return  new Promise(function(resolve, reject){
+      // navigator.geolocation.getCurrentPosition(
+      //   position => resolve(position),
+      //   err  => reject(err));
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+  };
+
+  getPosition().then(pos=> console.log(pos));
