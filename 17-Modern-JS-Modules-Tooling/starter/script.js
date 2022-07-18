a336b493297177d6d30e218950502a9a0ed35042
Simeon Raychev <simeon0022@gmail.com>
2022-07-18 14:49:57 +0300

    Exporting and Importing modules

---
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,5 @@
+//Importing module 
+import { addToCart} from './shoppingCart';
+console.log('Importing module!')
+
+addToCart(bread, 5);
---
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -0,0 +1,10 @@
+//Exporting module
+console.log('Exporting modele!');
+
+const shoppingCart = 10;
+const cart = [];
+//Exports always work in top level code
+export const addToCart = function(product, quantity){
+    cart.push({product, quantity});
+    console.log(`${quantity} ${product} added to cart!`);
+}
